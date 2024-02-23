338e08cdcd13defa2c48cba139bffeef3c03fa05
saliousow <saliousow@engineer.com>
2024-02-22 21:51:41 -0500

    Initial Commit

---
+++ sunundawal/sunundawalApp.swift
@@ -0,0 +1,17 @@
+//
+//  sunundawalApp.swift
+//  sunundawal
+//
+//  Created by Dj Typeu on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct sunundawalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
